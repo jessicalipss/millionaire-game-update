d4e9e07e983d328d190b6bd35eed39a8fdca956f
jessicalipss <110728236+jessicalipss@users.noreply.github.com>
2023-02-09 22:50:19 +0400

    Update home.js

--- home.js
+++ home.js
@@ -2,7 +2,7 @@ function redirect(){
 window.location.href = "testmcq.html";
 }
 function redirect1(){
-window.location.href = "login.html";
+window.location.href = "https://glittery-dragon-be41df.netlify.app/login.html";
 }
 function redirect2(){
 window.location.href = "form.html";
